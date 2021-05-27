5889c56a15f0eca37cc6a2186a0e35c9f2275267
Bennett Black <bennett.black012@gmail.com>
2021-05-27 18:04:27 -0500

    Publishing vendor files now works

--- src/AlpacaLaravelServiceProvider.php
+++ src/AlpacaLaravelServiceProvider.php
@@ -8,10 +8,19 @@ class AlpacaLaravelServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->publishes([
+
+            // Config
+            __DIR__ . '/config/alpaca.php' => config_path('alpaca.php'),
+        ]);
     }
 
     public function register()
     {
+        // config
+        // $this->mergeConfigFrom(__DIR__ . '/config/alpaca.php', 'alpaca');
+
+        // Facade
         $this->app->bind('alpaca-laravel', function () {
 
             return new Alpaca();
