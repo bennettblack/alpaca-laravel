e7780fe3b322298ce6097e2a6a9268f52cb7cdb1
Bennett Black <bennett.black012@gmail.com>
2021-05-27 20:53:05 -0500

    added account method

--- src/Alpaca.php
+++ src/Alpaca.php
@@ -2,13 +2,32 @@
 
 namespace bennettblack\alpacalaravel;
 
+use Illuminate\Support\Facades\Http;
+
 class Alpaca{
 
     /**
-     * test
+     * Get user account.
+     *
+     * @param string $attribute
+     * @param string $value
+     *
+     * @return Collection
      */
-    public function sayHi(){
-        return 'yeet';
+    public static function account(){
+
+        $endpoint = env('ALPACA_PAPER_ENDPOINT');
+        $uri = '/v2/account';
+
+        $headers = [
+            'APCA-API-KEY-ID' => env('ALPACA_PAPER_KEY'),
+            'APCA-API-SECRET-KEY' => env('ALPACA_PAPER_SECRET')
+        ];
+
+        $response = Http::withHeaders($headers)->get($endpoint . $uri)->collect();
+
+        return $response;
     }
+
 
 }
--- src/config/alpaca.php
+++ src/config/alpaca.php
@@ -1,3 +1,20 @@
 <?php
 
-// test
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Paper Trading Credentials
+    |--------------------------------------------------------------------------
+    |
+    | These are your Alpaca paper trading API key and secret
+    |
+    */
+
+    'paper_endpoint'    => env('ALPACA_PAPER_ENDPOINT'),
+    'paper_key'         => env('ALPACA_PAPER_KEY'),
+    'paper_secret'      => env('ALPACA_PAPER_SECRET'),
+
+
+
+];
