74ef1eabd9c2a53e41de0a6b00d4f63da7fb2024
Bennett Black <bennett.black012@gmail.com>
2021-05-31 11:32:34 -0500

    Added positions and orders.

--- src/Alpaca.php
+++ src/Alpaca.php
@@ -7,24 +7,69 @@ use Illuminate\Support\Facades\Http;
 class Alpaca{
 
     /**
-     * Get user account.
-     *
-     * @param string $attribute
-     * @param string $value
+     * Get user account information.
      *
      * @return Collection
      */
-    public static function account(){
+    public function account(){
 
-        $endpoint = env('ALPACA_PAPER_ENDPOINT');
         $uri = '/v2/account';
 
         $headers = [
-            'APCA-API-KEY-ID' => env('ALPACA_PAPER_KEY'),
-            'APCA-API-SECRET-KEY' => env('ALPACA_PAPER_SECRET')
+            'APCA-API-KEY-ID' => config('alpaca.paper_key'),
+            'APCA-API-SECRET-KEY' => config('alpaca.paper_secret')
         ];
 
-        $response = Http::withHeaders($headers)->get($endpoint . $uri)->collect();
+        $response = Http::withHeaders($headers)
+            ->get(config('alpaca.paper_endpoint'). $uri)
+            ->collect();
+
+        return $response;
+    }
+
+    /**
+     * Get users open positions. Optionally pass a symbol for a specific position.
+     *
+     * @param string $sybmol
+     *
+     * @return Collection
+     */
+    public function positions(String $symbol = null){
+
+        $uri = $symbol === null ? '/v2/positions' : '/v2/positions/' . $symbol;
+
+        $headers = [
+            'APCA-API-KEY-ID' => config('alpaca.paper_key'),
+            'APCA-API-SECRET-KEY' => config('alpaca.paper_secret')
+        ];
+
+        $response = Http::withHeaders($headers)
+            ->get(config('alpaca.paper_endpoint') . $uri)
+            ->collect();
+
+        return $response;
+    }
+
+    /**
+     * Get users open orders.
+     *
+     * @param string $sybmol
+     *
+     * @return Collection
+     */
+    public function orders()
+    {
+
+        $uri = '/v2/orders';
+
+        $headers = [
+            'APCA-API-KEY-ID' => config('alpaca.paper_key'),
+            'APCA-API-SECRET-KEY' => config('alpaca.paper_secret')
+        ];
+
+        $response = Http::withHeaders($headers)
+            ->get(config('alpaca.paper_endpoint') . $uri)
+            ->collect();
 
         return $response;
     }
